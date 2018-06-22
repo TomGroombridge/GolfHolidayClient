d1bbf03db11786bb2d2044b54d6edcebfb510c5d
Tom Groombridge <tom.groombridge@zopa.com>
2018-06-22 23:31:58 +0100

    Loading icon

--- src/loading.jsx
+++ src/loading.jsx
@@ -1,16 +1,55 @@
-import * as React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+  container: {
+  	position: 'absolute',
+    top: '38%',
+    marginLeft: '36%',
+  },
 });
 
 class Loading extends React.Component {
 
-	render() {
-		return (
-			<h1>Loading</h1>
-		);
-	}
+	state = {
+    completed: 0,
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  timer;
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.container}>
+        <CircularProgress
+          className={classes.progress}
+          variant="determinate"
+          size={50}
+          value={this.state.completed}
+        />
+      </div>
+    );
+  }
+
+
 }
 
 export default withStyles(styles)(Loading);
