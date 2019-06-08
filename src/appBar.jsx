2ca0d7b55ef97cfbe494dc210f22ef4b0251c64b
TomGroombridge <groombridgetom@gmail.com>
2019-06-08 14:45:40 +0100

    app bar

--- src/appBar.jsx
+++ src/appBar.jsx
@@ -6,17 +6,17 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 
-function AppBar(props) {  
+function SimpleAppBar(props) {  
   return (    
-    <AppBar position="static" color="default">
-      <Toolbar>
-        <Typography variant="title" color="inherit">
-          Esquilo Morto 2019
-        </Typography>
-      </Toolbar>
-    </AppBar>    
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Esquilo Morto 2019
+          </Typography>
+        </Toolbar>
+      </AppBar>    
   );
 }
 
 
-export default (AppBar);
+export default (SimpleAppBar);
