4f8ac41c47fca892bc860f8025f93e5f1c537971
TomGroombridge <groombridgetom@gmail.com>
2019-06-11 17:25:43 +0100

    remove ignore files

---
+++ src/teamPanel.jsx
@@ -0,0 +1,62 @@
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import Badge from '@material-ui/core/Badge';
+import Typography from '@material-ui/core/Typography';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '10.00%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+    flexBasis: '70.00%',
+  },
+  thirdHeading: {
+  	fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+    flexBasis: '20.00%',
+  },
+  margin: {
+    margin: theme.spacing.unit * 2,
+  },
+  padding: {
+    padding: `0 ${theme.spacing.unit * 2}px`,
+  },
+});
+
+
+class TeamPanel extends React.Component {
+
+  render() {
+    const { expanded, panelNumber, classes, team, position} = this.props;    
+    return (
+      <ExpansionPanel expanded={true} onChange={this.props.handleChange(panelNumber)}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>{position}</Typography>
+          <Typography className={classes.secondaryHeading}>{team.team_players.join(', ')}</Typography>
+          <Typography className={classes.thirdHeading}>{team.score}pts</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>          
+            <React.Fragment>
+                <span style={{marginRight: '10px'}}>Course: {team.course}</span>            
+                <span>Date: {team.date}</span>            
+            </React.Fragment>          
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    );
+  }
+}
+
+TeamPanel.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TeamPanel);
